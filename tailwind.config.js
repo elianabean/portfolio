ae368bc8e0e0377e20002adb0261ca664c090d3e
elianabean <ewanginspiration@gmail.com>
2024-01-23 00:07:20 -0500

    updated spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,14 @@ module.exports = {
   content: ['./build/*.html'],
   theme: {
     extend: {
+      spacing: {
+        '76': '304px',
+      },
+
       fontFamily: {
         workSans: ["Work Sans", "sans-serif"],
       }
+
       
     },
   },
