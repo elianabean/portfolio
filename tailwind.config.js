dbbe4304c4db98969e2385289d8f0b823a285ffc
elianabean <ewanginspiration@gmail.com>
2024-02-14 20:35:53 -0500

    Added landing page animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,45 @@ module.exports = {
         '76': '304px',
       },
 
+      keyframes: {
+        wiggle: {
+            '0%, 100%': {
+                transform: 'rotate(-3deg)'
+            },
+            '50%': {
+                transform: 'rotate(3deg)'
+            },
+        },
+
+        'fade-in-down': {
+          '0%': {
+              opacity: '0',
+              transform: 'translateY(-30px)'
+          },
+          '100%': {
+              opacity: '1',
+              transform: 'translateY(0)'
+          },
+      },
+
+      floating: {
+        '0%, 100%': {
+          transform: 'translateY(0)'
+        },
+        '50%': {
+            transform: 'translateY(-10px)'
+        },
+      },
+    },
+    animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+        'fade-in-down': '1.5s fade-in-down ease-out',
+        'fade-in-down2': '1s fade-in-down ease-out',
+        'fade-in-down3': '4s fade-in-down ease-out',
+        floating: '4s floating infinite',
+        floating2: '4s floating 0.5s infinite',
+    },
+
       fontFamily: {
         workSans: ["Work Sans", "sans-serif"],
       }, 
