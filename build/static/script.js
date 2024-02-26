0675ce6e3c81a53f0b86480500c6d57ff139705b
elianabean <ewanginspiration@gmail.com>
2024-02-25 23:04:24 -0500

    More scroll animation

--- build/static/script.js
+++ build/static/script.js
@@ -1,34 +1,39 @@
-var controller = new ScrollMagic.Controller();
+AOS.init();
 
-gsap.to(".headshot", {
-    scrollTrigger: {
-        trigger: ".headshot",
-        toggleActions: "restart pause reverse pause",
-        start: "top center",
-        end:"top 100px",
-        scrub: 2,
-        markers: false,
-    },
-    x:100,
-    rotation: 360,
-    ease:"none",
-    duration: 3
+let mm = gsap.matchMedia();
 
-});
+mm.add("(min-width: 800px)", () => {
+    gsap.from(".headshot", {
+        scrollTrigger: {
+            trigger: ".headshot",
+            toggleActions: "restart pause reverse pause",
+            start: "top bottom",
+            end:"top center",
+            scrub: 2,
+            markers: false,
+        },
+        x:"-100%",
+        rotation: 360,
+        ease:"none",
+        duration: 3
+    
+    });
+    
+    gsap.from("#about", {
+        scrollTrigger: {
+            trigger: ".headshot",
+            toggleActions: "restart pause reverse pause",
+            start: "top bottom",
+            end:"top center",
+            scrub: 2,
+            markers: false,
+        },
+        x:"-100%",
+        ease:"none",
+        duration: 0.5
+    
+    });
 
-gsap.from("#about", {
-    scrollTrigger: {
-        trigger: ".headshot",
-        toggleActions: "restart pause reverse pause",
-        start: "top bottom",
-        end:"top center",
-        scrub: 2,
-        markers: false,
-    },
-    x:"-100%",
-    ease:"none",
-    duration: 0.5
-
-});
+  });
 
 /*ScrollReveal().reveal('#about', { delay: 300, duration: 500});*/
