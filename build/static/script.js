eefd703263173aca588ec47a3f7c72bc51fbff63
elianabean <ewanginspiration@gmail.com>
2024-03-02 15:51:03 -0500

    Added Dark Mode functionality

--- build/static/script.js
+++ build/static/script.js
@@ -34,4 +34,41 @@ mm.add("(min-width: 800px)", () => {
     
     });
 
+});
+
+/*https://www.typeitjs.com/ */
+new TypeIt("#name", {
+    speed: 100,
+    waitUntilVisible: true,
+    afterComplete: function (instance) {
+        instance.destroy();
+    }
+}).go();
+
+
+
+/*from https://cruip.com/implementing-tailwind-css-dark-mode-toggle-with-no-flicker/ */
+const lightSwitches = document.querySelectorAll('.light-switch');
+if (lightSwitches.length > 0) {
+  lightSwitches.forEach((lightSwitch, i) => {
+    if (localStorage.getItem('dark-mode') === 'true') {
+      lightSwitch.checked = true;
+    }
+    lightSwitch.addEventListener('change', () => {
+      const { checked } = lightSwitch;
+      lightSwitches.forEach((el, n) => {
+        if (n !== i) {
+          el.checked = checked;
+        }
+      });
+      if (lightSwitch.checked) {
+        console.log("checked");
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('dark-mode', true);
+      } else {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('dark-mode', false);
+      }
+    });
   });
+}
